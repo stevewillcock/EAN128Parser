0d8af47cc6a6b9f59c6c237ef4016af38e3fdc71
Stephen Willcock <steve@rowanbeach.com>
2015-12-03 13:14:47 +0000

    Refactoring a bit

--- EAN128Parser/EAN128Barcode.cs
+++ EAN128Parser/EAN128Barcode.cs
@@ -1,51 +1,48 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Drkstr.EAN128
 {
     public class EAN128Barcode
     {
-        private List<ElementString> _elements;
-        /// <summary>
-        /// Recupera tutti gli elementi correnti
-        /// </summary>
-        public List<ElementString> Elements
-        {
-            get { return _elements; }
-        }
-
         public EAN128Barcode()
         {
-            _elements = new List<ElementString>();
+            Elements = new List<ElementString>();
         }
 
         /// <summary>
-        /// Aggiunge un barcode al corrente EAN128
+        ///     Recupera tutti gli elementi correnti
+        /// </summary>
+        public List<ElementString> Elements { get; }
+
+        /// <summary>
+        ///     Aggiunge un barcode al corrente EAN128
         /// </summary>
         /// <param name="rawData">Il dat grezzo del barcode</param>
-        /// <exception cref="Drkstr.Ean128.EAN128Exception" /> se si verifica un errore di parsing
+        /// <exception>
+        ///     <cref>Drkstr.Ean128.EAN128Exception</cref>
+        /// </exception>
+        /// se si verifica un errore di parsing
         public void AddBarcode(byte[] rawData)
         {
-            EAN128Tokenizer t = new EAN128Tokenizer(rawData);
+            var t = new EAN128Tokenizer(rawData);
             AddToken(t);
         }
 
         /// <summary>
-        /// Aggiunge un barcode al corrente EAN128
+        ///     Aggiunge un barcode al corrente EAN128
         /// </summary>
         /// <param name="asciiData">Il barcode decodificato in ASCII</param>
         public void AddBarcode(char[] asciiData)
         {
-            EAN128Tokenizer t = new EAN128Tokenizer(asciiData);
+            var t = new EAN128Tokenizer(asciiData);
             AddToken(t);
         }
 
         /// <summary>
-        /// Aggiunge un token al barcode se questo non è stato ancora aggiunto
+        ///     Aggiunge un token al barcode se questo non è stato ancora aggiunto
         /// </summary>
-        /// <param name="t">L'EAN128Tokenizer da inserire</param>
+        /// <param name="t">EAN128Tokenizer da inserire</param>
         public void AddToken(EAN128Tokenizer t)
         {
             while (t.HasMoreToken())
@@ -53,27 +50,20 @@ namespace Drkstr.EAN128
                 var nextToken = t.NextToken();
                 var elements = (from e in Elements where e.Data.Equals(nextToken.Data) select e);
 
-                if (elements.Count() == 0)
+                if (!elements.Any())
                 {
                     Elements.Add(nextToken);
-                } 
+                }
             }
         }
 
         /// <summary>
-        /// Stampa il barcode contenuto nel formato (AI1)Data1(AI2)Data2(...
+        ///     Stampa il barcode contenuto nel formato (AI1)Data1(AI2)Data2(...
         /// </summary>
         /// <returns></returns>
         public override string ToString()
         {
-            string res = String.Empty;
-
-            foreach (ElementString es in Elements)
-            {
-                res += es.ToString();
-            }
-
-            return res;
+            return Elements.Aggregate(string.Empty, (current, es) => current + es.ToString());
         }
     }
 }
--- EAN128Parser/ElementString.cs
+++ EAN128Parser/ElementString.cs
@@ -1,61 +1,10 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Drkstr.EAN128
 {
     public class ElementString
     {
         private ElementStringFormat _format;
-        public ElementStringFormat Format
-        {
-            get { return _format; }
-            set
-            {
-                _format = value;
-                _ai = value.AI;
-
-                if (_format.NumberOfDecimal >= 0)
-                {
-                    _ai += Format.NumberOfDecimal;
-                }
-            }
-        }
-
-        private string _ai;
-        public string AI
-        {
-            get
-            {
-                return _ai;
-            }
-            set
-            {
-                _ai = value;
-            }
-        }
-
-        public string Description
-        {
-            get
-            {
-                return _format.Description;
-            }
-        }
-
-        private string _data;
-        public string Data
-        {
-            get
-            {
-                return _data;
-            }
-            set
-            {
-                _data = value;
-            }
-        }
 
         public ElementString(ElementStringFormat esf)
         {
@@ -63,59 +12,62 @@ namespace Drkstr.EAN128
             Data = "";
         }
 
-        /// <summary>
-        /// Esegue il parsing di Data e restituisce il valore in DateTime
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="Drkstr.EAN128.EAN128Exception" /> Se il valore dei dati non corrisponde a una data
+        public ElementStringFormat Format
+        {
+            get { return _format; }
+            set
+            {
+                _format = value;
+                AI = value.AI;
+
+                if (_format.NumberOfDecimal >= 0)
+                {
+                    AI += Format.NumberOfDecimal;
+                }
+            }
+        }
+
+        public string AI { get; set; }
+
+        public string Description => _format.Description;
+
+        public string Data { get; set; }
+
         public DateTime ParseDate()
         {
-            if (GS1Algorithm.StringEquals(_format.AI, new string[] { "11", "12", "13", "15", "17" }))
+            if (GS1Algorithm.StringEquals(_format.AI, new[] {"11", "12", "13", "15", "17"}))
             {
-                int y = Convert.ToInt32("20" + Data.Substring(0, 2));
-                int m = Convert.ToInt32(Data.Substring(2, 2));
-                int d = Convert.ToInt32(Data.Substring(4, 2));
+                var y = Convert.ToInt32("20" + Data.Substring(0, 2));
+                var m = Convert.ToInt32(Data.Substring(2, 2));
+                var d = Convert.ToInt32(Data.Substring(4, 2));
 
                 if (d == 0) d = 1; // If first day not specified
 
                 return new DateTime(y, m, d);
             }
-            else
-            {
-                throw new EAN128Exception("AI("+_format.AI+") is not a date");
-            }
+            throw new EAN128Exception("AI(" + _format.AI + ") is not a date");
         }
 
-        /// <summary>
-        /// Esegue il parsing di Data e restituisce il valore intero
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="FormatException" /> Se il valore dei dati non corrisponde a un intero
         public int ParseInt()
         {
             return Convert.ToInt32(Data);
         }
 
-        /// <summary>
-        /// Esegue il parsing di Data e restituisce il valore in double
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="FormatException" /> Se il valore dei dati non corrisponde a un double
         public double ParseDouble()
         {
-            return Convert.ToDouble(Data); ;
+            return Convert.ToDouble(Data);
         }
 
         public override string ToString()
         {
-            string ai = AI;
+            var ai = AI;
 
             if (Format.NumberOfDecimal >= 0)
             {
                 ai += Format.NumberOfDecimal;
             }
 
-            return String.Format("({0}){1}", ai, Data);
+            return $"({ai}){Data}";
         }
     }
 }
